9e58ec4870a8d0206775e2cf0c12106663895c7c
Marina Kam <kam.marvic@gmail.com>
2019-04-18 12:30:09 +0300

    Updates

--- precache-manifest.f45571de87b845e047c27550097bcebd.js
+++ precache-manifest.f45571de87b845e047c27550097bcebd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/IntroToReactRedux/static/js/runtime~main.e150f7eb.js"
   },
   {
-    "revision": "fc5f696993a08396c44e",
-    "url": "/IntroToReactRedux/static/js/main.fc5f6969.chunk.js"
+    "revision": "19ac4e9901882ca9416f",
+    "url": "/IntroToReactRedux/static/js/main.19ac4e99.chunk.js"
   },
   {
     "revision": "b12f704e3c0c1bbadb64",
     "url": "/IntroToReactRedux/static/js/2.b12f704e.chunk.js"
   },
   {
-    "revision": "fc5f696993a08396c44e",
+    "revision": "19ac4e9901882ca9416f",
     "url": "/IntroToReactRedux/static/css/main.2c51aacd.chunk.css"
   },
   {
-    "revision": "0331b117651768c2357f51f1918c72e8",
+    "revision": "d95c1c0d6944486e17d790cbf1a185c8",
     "url": "/IntroToReactRedux/index.html"
   }
 ];
